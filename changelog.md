1973717dd52f2eaee5ce7ac96241b259afe8adbe
EaSTw1Nd <34911309+601t@users.noreply.github.com>
2024-09-22 22:24:18 +0900

    Add files via upload

---
+++ changelog.md
@@ -0,0 +1,6 @@
+### v1.1 - 9.21.2024
+* Update to MMT-Extended v3.7
+* Support KernelSU/Apatch? (Not tested)
+
+### v1.0 - 7.13.2024
+* Initial release
--- customize.sh
+++ customize.sh
@@ -57,7 +57,7 @@ set_permissions() {
   
   # set_perm $MODPATH/system/lib/libart.so 0 0 0644
   # set_perm /data/local/tmp/file.txt 0 0 644
-  set_perm $MODPATH/system/usr/keylayout/gpio-keys.kl 0 0 0644 
+  set_perm /system/usr/keylayout/gpio-keys.kl 0 0 0644 
 }
 
 ##########################################################################################
